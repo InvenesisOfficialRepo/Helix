049b2ba32e81e3f5d1a5bf083332d19357011037
Maxime Alessandri <Maxime@invenesis.com>
2026-07-03 08:39:35 +0200

    v1.1.12: Fix QC and replicates dilution concentration logic

--- src/widgets_core/services/ExperimentJsonSerializer.cpp
+++ src/widgets_core/services/ExperimentJsonSerializer.cpp
@@ -331,9 +331,26 @@ void addConcentrationsToExperimentJson(QJsonObject &root, const QJsonObject& qcP
                 });
 
                 double currentConc = startConc;
+                int lastCol = -1;
                 for (const QString &w : wlist) {
-                    concObj[w] = currentConc;
-                    currentConc /= dfForChain;
+                    if (type == "qc") {
+                        QString qcType = root.value("qc_plate_type").toString();
+                        QJsonObject qcDef = qcPlatesJson.value(qcType).toObject();
+                        QString rowLetter = w.left(1).toUpper();
+                        QJsonObject rowDef = qcDef.value(rowLetter).toObject();
+                        if (!rowDef.isEmpty() && rowDef.value("standard").toString() == baseCmp) {
+                            concObj[w] = rowDef.value("conc").toVariant().toDouble();
+                        } else {
+                            concObj[w] = startConc;
+                        }
+                    } else {
+                        int col = w.mid(1).toInt();
+                        if (lastCol != -1 && col != lastCol) {
+                            currentConc /= dfForChain;
+                        }
+                        concObj[w] = currentConc;
+                        lastCol = col;
+                    }
                 }
             }
             plateObj["concentrations"] = concObj;
